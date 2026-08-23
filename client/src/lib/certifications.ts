e8a15a0210f72cc369150c8cb4dae9a589adf9c4
binayatripathi <binayatripathi@gmail.com>
2026-08-22 22:12:15 -0700

    Add Roberto's EVITP cert #4052605 (valid through 2029-07-10)

--- client/src/lib/certifications.ts
+++ client/src/lib/certifications.ts
@@ -1,7 +1,7 @@
 // Company certifications shown across the site.
 // TODO: insert Roberto's EVITP certification number when he provides it.
 // It renders on the residential trust bar and commercial credentials line.
-export const EVITP_CERT_NUMBER = "";
+export const EVITP_CERT_NUMBER = "4052605";
 
 export const EVITP_LABEL = EVITP_CERT_NUMBER
   ? `EVITP Certified #${EVITP_CERT_NUMBER}`
